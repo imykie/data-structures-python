527c2c5c42009e81c6cb43d65169b7f2cb3b1122
Michael Olatunji <tundexmike@gmail.com>
2020-12-08 19:46:07 +0100

    adding queue using array

---
+++ Queue/__init__.py
@@ -0,0 +1,1 @@
+from Queue.queue import *
--- Queue/queue.py
+++ Queue/queue.py
@@ -18,3 +18,20 @@ class Queue:
     def peek(self) -> int:
         return self.linked_list.head.data if self.linked_list is not None else None
 
+
+class QueueArr:
+    def __init__(self):
+        self.data = []
+
+    def is_empty(self):
+        return False if self.data.size > 0 else False
+
+    def enqueue(self, data: int) -> None:
+        self.data.append(data)
+
+    def dequeue(self) -> None:
+        self.data.pop(0)
+
+    def peek(self) -> int:
+        return self.data[0] if len(self.data) > 0 else None
+
