0c26789b93b6c37521250d2f6f8cc9c613addb2b
Michael Olatunji <tundexmike@gmail.com>
2020-12-08 19:42:33 +0100

    fix: stack

--- Stack/stack.py
+++ Stack/stack.py
@@ -9,14 +9,14 @@ class Stack:
     def is_empty(self) -> bool:
         return False if self.linked_list.size > 0 else True
 
-    def push(self, data) -> None:
+    def push(self, data: int) -> None:
         self.linked_list.add_last(data)
 
     def pop(self) -> None:
         self.linked_list.remove_last()
 
     def peek(self) -> int:
-        return self.linked_list.tail.data
+        return self.linked_list.tail.data if self.linked_list.head is not None else None
 
 
 class StackArr:
@@ -26,11 +26,11 @@ class StackArr:
     def is_empty(self) -> bool:
         return False if len(self.data) > 0 else True
 
-    def push(self, data) -> None:
+    def push(self, data: int) -> None:
         self.data.append(data)
 
     def pop(self) -> None:
         self.data.pop()
 
     def peek(self) -> int:
-        return self.data[len(self.data) - 1]
+        return self.data[len(self.data) - 1] if len(self.data) > 0 else None
