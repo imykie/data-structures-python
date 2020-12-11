052b04a8e46b24a3f5c6cb62d7f45756c4d1a4b8
Michael Olatunji <tundexmike@gmail.com>
2020-12-11 09:57:00 +0100

    adding Red Black Tree Node

---
+++ Trees/RedBlackTree/RBTreeNode.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+
+class RBTreeNode:
+    def __init__(self,
+                 data: int = 0,
+                 left: RBTreeNode = None,
+                 right: RBTreeNode = None,
+                 parent: RBTreeNode = None,
+                 black: bool = False,
+                 is_left_child: bool = False
+                 ) -> None:
+        self.data = data
+        self.left = left
+        self.right = right
+        self.parent = parent
+        self.black = black
+        self.is_left_child = is_left_child
+
+    def get_data(self) -> int:
+        return self.data
+
+    def set_data(self, key=0, data: int = 0, left: RBTreeNode = None, right: RBTreeNode = None,
+                 parent: RBTreeNode = None, is_black: bool = False, is_left_child: bool = False) -> None:
+        self.data = data
+
+    def get_right(self, right: RBTreeNode) -> None:
+        self.right = right
+
+    def set_right(self, right: RBTreeNode) -> None:
+        self.right = right
+
+    def get_left(self, left: RBTreeNode) -> None:
+        self.left = left
+
+    def set_left(self, left: RBTreeNode) -> None:
+        self.left = left
+
+    def get_parent(self, parent: RBTreeNode) -> None:
+        self.parent = parent
+
+    def set_parent(self, parent: RBTreeNode) -> None:
+        self.parent = parent
