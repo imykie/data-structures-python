0cb6ad6cb43033f560beb93a4e446855c0b54211
Michael Olatunji <tundexmike@gmail.com>
2023-11-19 01:29:39 +0100

    fix: union find logic

--- Graph/UnionFind/union_find.py
+++ Graph/UnionFind/union_find.py
@@ -15,21 +15,22 @@ class UnionFind:
         self.rank = [1] * self.size
 
     def find(self, x: int) -> int:
-        while x != self.root[x]:
+        if x != self.root[x]:
             self.root[x] = self.find(self.root[x])
         return self.root[x]
 
     def union(self, x: int, y: int) -> None:
         root_x = self.find(x)
         root_y = self.find(y)
-        if root_x != root_y:
-            if self.rank[root_x] > self.rank[root_y]:
-                self.root[root_y] = root_x
-            elif self.rank[root_y] > self.rank[root_x]:
-                self.root[root_x] = root_y
-            else:
-                self.root[root_y] = root_x
-                self.rank[root_x] += 1
+        if root_x == root_y:
+            return False
 
-    def connected(self, x: int, y: int) -> bool:
-        return self.find(x) == self.find(y)
+        if self.rank[root_x] > self.rank[root_y]:
+            self.root[root_y] = root_x
+        elif self.rank[root_x] < self.rank[root_y]:
+            self.root[root_x] = root_y
+        else:
+            self.root[root_y] = root_x
+            self.rank[root_x] += 1
+
+        return True
